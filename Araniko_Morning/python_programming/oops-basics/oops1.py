04a49e89d483a07bf4b98cd2f57e8310c1496f28
anees555 <aneeshdahal5@gmail.com>
2025-12-14 19:44:20 +0545

    Numpy session started

--- Araniko_Morning/python_programming/oops-basics/oops1.py
+++ Araniko_Morning/python_programming/oops-basics/oops1.py
@@ -42,31 +42,58 @@
 # child1 = Child()
 # print(child1.hair_color)
 
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-    def introduce(self):
-        print(f"I am {self.name} and I am {self.age} year old.")
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#     def introduce(self):
+#         print(f"I am {self.name} and I am {self.age} year old.")
 
-class Student(Person):
-    def __init__(self, name, age, roll):
-        super().__init__(name, age)
-        self.roll = roll
-    def student_info(self):
-        print(f"I am {self.name}, i am student , my age is {self.age}, my roll number is {self.roll}")
+# class Student(Person):
+#     def __init__(self, name, age, roll):
+#         super().__init__(name, age)
+#         self.roll = roll
+#     def student_info(self):
+#         print(f"I am {self.name}, i am student , my age is {self.age}, my roll number is {self.roll}")
 
-class Teacher(Person):
-    def __init__(self, name, age, subject):
-            super().__init__(name, age)
-            self.subject = subject
+# class Teacher(Person):
+#     def __init__(self, name, age, subject):
+#             super().__init__(name, age)
+#             self.subject = subject
 
-    def teach(self):
-            print(f"I teach {self.subject}")
+#     def teach(self):
+#             print(f"I teach {self.subject}")
 
-## Creating object
-s1 = Student("Ram", 30, "007")
-t1 = Teacher("Sujan", 40, "Math")
-s1.student_info()
-s1.introduce()
-t1.teach()
+# ## Creating object
+# s1 = Student("Ram", 30, "007")
+# t1 = Teacher("Sujan", 40, "Math")
+# s1.student_info()
+# s1.introduce()
+# t1.teach()
+
+
+# #### 
+# class Student:
+#     def __init__(self): ### constructor
+#         print("I am called")
+#         print(self)
+
+
+# s1 = Student()
+# print(s1)
+# s2 = Student()
+# print(s2)
+# # # __init__() # no need call , call automatically at the time of object creation
+
+
+
+class Sum:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def calculate_sum(self):
+        print(f"The sum is {self.a + self.b}")
+
+s1 = Sum(34, 56)
+s1.calculate_sum()
